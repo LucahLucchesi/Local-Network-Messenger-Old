814bf71f6fb1c32ab383a8f1cfd76ea9720bfde4
LucahLucchesi <lucahlucchesi@gmail,com>
2023-04-26 09:58:32 -0500

    Created base gui

--- MessengerJavaFX/src/application/Main.java
+++ MessengerJavaFX/src/application/Main.java
@@ -1,20 +1,53 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage app) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			
+			TextArea chatBox = new TextArea();
+			chatBox.setEditable(false);
+			chatBox.setMaxSize(400, 250);
+			
+			//this is how we add text to the chatbox. a new line must follow every message
+			chatBox.appendText("Hello\n");
+			chatBox.appendText("Hi\n");
+			for(int i = 0; i < 30; i++) {
+				chatBox.appendText("Hi\n");
+			}
+			
+			TextField messageField = new TextField();
+			messageField.setEditable(true);
+			
+			Button sendButton = new Button("send");
+			HBox test = new HBox(messageField, sendButton);
+			VBox test2 = new VBox(chatBox, test);
+			test2.setPadding(new Insets(10, 10, 10, 10));
+			
+//			GridPane pane = new GridPane();
+//			pane.setPadding(new Insets(10, 10, 10, 10));
+//			pane.add(chatBox, 0, 0);
+//			pane.add(messageField, 0, 1);
+//			pane.add(sendButton, 1, 1);
+			Scene scene = new Scene(test2, 400, 300);
+			
+			
+			app.setScene(scene);
+			app.setTitle("Private Messenger");
+			app.show();
 			
 			
 		} catch(Exception e) {

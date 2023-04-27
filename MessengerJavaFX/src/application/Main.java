455a880595bbcf5c05661c5ca3fcdfad0e220bde
LucahLucchesi <lucahlucchesi@gmail.com>
2023-04-27 15:57:36 -0500

    Radio button functionality

--- MessengerJavaFX/src/application/Main.java
+++ MessengerJavaFX/src/application/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
@@ -20,6 +21,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage app) {
 		try {
+			//Info Window Setup
 			RadioButton hostButton = new RadioButton("Host");
 			RadioButton joinButton = new RadioButton("Join");
 			ToggleGroup group = new ToggleGroup();
@@ -29,6 +31,7 @@ public class Main extends Application {
 			TextField userField = new TextField();
 			TextField ipField = new TextField();
 			TextField portField = new TextField();
+			Button confirm = new Button("Confirm");
 			Insets inset = new Insets(10);
 			
 			hostButton.setToggleGroup(group);
@@ -48,54 +51,67 @@ public class Main extends Application {
 			ipField.setPadding(inset);
 			portField.setEditable(false);
 			portField.setPrefColumnCount(5);
+			portField.setPadding(inset);
+			
+			confirm.setDisable(true);
 			
 			HBox radioButtons = new HBox(hostButton, joinButton);
 			HBox username = new HBox(userLabel, userField);
 			HBox ip = new HBox(ipLabel, ipField);
 			HBox port = new HBox(portLabel, portField);
 			
-			Button confirm = new Button("Confirm");
-			
-			VBox infoWindow = new VBox(radioButtons, username, ip, port, confirm);
+			VBox infoWindow = new VBox(radioButtons, username, port, ip, confirm);
 			Scene infoScene = new Scene(infoWindow, 300, 300);
-			Scene chatBoxScene = new Scene(new Pane());
+			
+			//Chat window setup
+			TextArea chatBox = new TextArea();
+			TextField messageField = new TextField();
+			Button sendButton = new Button("send");
+			
+			chatBox.setEditable(false);
+			chatBox.setMaxSize(400, 250);
+			messageField.setEditable(true);
+			
+			HBox messageSender = new HBox(messageField, sendButton);
+			VBox chatWindow = new VBox(chatBox, messageSender);
+			chatWindow.setPadding(new Insets(10, 10, 10, 10));
+			
+			Scene chatBoxScene = new Scene(chatWindow);
+			
 			app.setScene(infoScene);
 			app.setTitle("Info");
 			app.show();
 			
+			hostButton.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					userField.setEditable(true);
+					portField.setEditable(true);
+					ipField.setEditable(false);
+					ipField.clear();
+					confirm.setDisable(false);
+					
+				}
+			});
+			joinButton.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					userField.setEditable(true);
+					portField.setEditable(true);
+					ipField.setEditable(true);
+					confirm.setDisable(false);
+				}
+			});
 			confirm.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
 					app.close();
 					app.setScene(chatBoxScene);
+					app.setTitle("MessengerFX");
 					app.show();
 					
 				}
 			});
-			
-//			TextArea chatBox = new TextArea();
-//			chatBox.setEditable(false);
-//			chatBox.setMaxSize(400, 250);
-//			
-//			//this is how we add text to the chatbox. a new line must follow every message
-//			chatBox.appendText("Hello\n");
-//			chatBox.appendText("Hi\n");
-//			
-//			TextField messageField = new TextField();
-//			messageField.setEditable(true);
-//			
-//			Button sendButton = new Button("send");
-//			HBox test = new HBox(messageField, sendButton);
-//			VBox test2 = new VBox(chatBox, test);
-//			test2.setPadding(new Insets(10, 10, 10, 10));
-//			
-//			Scene scene = new Scene(test2, 400, 300);
-//			
-//			
-//			app.setScene(scene);
-//			app.setTitle("Private Messenger");
-//			app.show();
-			
 			
 		} catch(Exception e) {
 			e.printStackTrace();

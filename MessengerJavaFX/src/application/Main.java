b8d946abcc223d5910ba5fcfe55e9463f0d4668c
LucahLucchesi <lucahlucchesi@gmail.com>
2023-04-30 13:13:57 -0500

    JavaDoc for main

--- MessengerJavaFX/src/application/Main.java
+++ MessengerJavaFX/src/application/Main.java
@@ -18,6 +18,12 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
+/**
+ * @author Lucah L, Alex B.
+ * @section CS145-001
+ * 
+ * Main creates the Info Box and Chat Box GUIs, as well as handling all events.
+ */
 public class Main extends Application {
 	String username;
 	int port;
@@ -25,6 +31,9 @@ public class Main extends Application {
 	Server hostServer = null;
 	Client client = null;
 	
+	/**
+	 *
+	 */
 	@Override
 	public void start(Stage app) {
 		try {
@@ -39,62 +48,60 @@ public class Main extends Application {
 			TextField ipField = new TextField();
 			TextField portField = new TextField();
 			Button confirm = new Button("Confirm");
-			
+			//Component settings
 			hostButton.setToggleGroup(group);
 			joinButton.setToggleGroup(group);
+			userField.setEditable(false);
+			portField.setEditable(false);
+			ipField.setEditable(false);
+			userField.setPrefColumnCount(8);
+			portField.setPrefColumnCount(8);
+			ipField.setPrefColumnCount(8);
+			confirm.setDisable(true);
+			//Component padding
 			hostButton.setPadding(new Insets(10));
 			joinButton.setPadding(new Insets(10));
-			
 			userLabel.setPadding(new Insets(0, 10, 0, 10));
 			portLabel.setPadding(new Insets(0, 41, 0, 10));
 			ipLabel.setPadding(new Insets(0, 25, 0, 10));
-			
-			userField.setEditable(false);
-			userField.setPrefColumnCount(8);
-			portField.setEditable(false);
-			portField.setPrefColumnCount(8);
-			ipField.setEditable(false);
-			ipField.setPrefColumnCount(8);
-			
-			confirm.setDisable(true);
-			
+			//Component groups
 			HBox radioButtons = new HBox(hostButton, joinButton);
 			HBox usernameBox = new HBox(userLabel, userField);
 			HBox ipBox = new HBox(ipLabel, ipField);
 			HBox portBox = new HBox(portLabel, portField);
-			
 			VBox infoWindow = new VBox(radioButtons, usernameBox, portBox, ipBox, confirm);
-			
+			//Component group padding
 			usernameBox.setPadding(new Insets(0, 0, 5, 0));
 			ipBox.setPadding(new Insets(0, 0, 5, 0));
 			portBox.setPadding(new Insets(0, 0, 5, 0));
 			infoWindow.setPadding(new Insets(0, 10, 10, 10));
-			
+			//Info window scene
 			Scene infoScene = new Scene(infoWindow, 220, 170);
 			
 			//Chat window setup
 			TextArea chatBox = new TextArea();
 			TextField messageField = new TextField();
 			Button sendButton = new Button("Send");
-			
+			//Component settings
 			chatBox.setEditable(false);
 			chatBox.setMaxSize(400, 250);
 			chatBox.setWrapText(true);
 			messageField.setEditable(true);
 			messageField.setPrefColumnCount(20);
 			sendButton.setDefaultButton(true);
-			
+			//Component groups
 			HBox messageSender = new HBox(messageField, sendButton);
 			VBox chatWindow = new VBox(chatBox, messageSender);
+			//Component group padding
 			messageSender.setPadding(new Insets(10, 0, 0, 0));
 			chatWindow.setPadding(new Insets(10, 10, 10, 10));
-			
+			//Chat window scene
 			Scene chatBoxScene = new Scene(chatWindow);
-			
+			//Stage is initialized with the info window scene
 			app.setScene(infoScene);
 			app.setTitle("Info");
 			app.show();
-			
+			//Safely closes any open server upon closing the GUI window
 			app.setOnCloseRequest(new EventHandler<WindowEvent>() {
 				@Override
 				public void handle(WindowEvent t) {
@@ -110,7 +117,8 @@ public class Main extends Application {
 				}
 				
 			});
-			
+			//When the host radio button is activated, the Username and Port fields enabled for input. The IP field is disabled
+			//because the host does not need to enter their own IP address
 			hostButton.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
@@ -121,6 +129,7 @@ public class Main extends Application {
 					confirm.setDisable(false);
 				}
 			});
+			//When the join radio button is activated, all info fields are enabled for input
 			joinButton.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
@@ -130,6 +139,8 @@ public class Main extends Application {
 					confirm.setDisable(false);
 				}
 			});
+			//When the confirm button is pressed, the global variables for username, port, and ip are assingned with the text
+			//in the info fields. Then the scene swaps to the chat box and a new connection is made.
 			confirm.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
@@ -142,6 +153,7 @@ public class Main extends Application {
 							app.setScene(chatBoxScene);
 							app.setTitle("MessengerFX");
 							app.show();
+							//new server object is made
 							hostServer = new Server(port, chatBox);
 							new Thread(hostServer).start();
 						}catch(IOException e) {
@@ -156,6 +168,7 @@ public class Main extends Application {
 							app.setScene(chatBoxScene);
 							app.setTitle("MessengerFX");
 							app.show();
+							//new client object is made
 							client = new Client(ip, port, chatBox);
 							new Thread(client).start();
 						}catch(IOException e) {
@@ -165,6 +178,8 @@ public class Main extends Application {
 					}
 				}
 			});
+			//Upon pressing the send button, any text in the message field will be appended to the chat box, and then
+			//the message is sent to the host/client
 			sendButton.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
